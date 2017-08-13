38c803bba39b6915bac7b0c417b7958e1501324b
RoshniPainkra <30731068+RoshniPainkra@users.noreply.github.com>
2017-08-13 17:50:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,72 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article={
+    title: 'Article-one',
+    heading:'Article-1',
+    date: '10 April,2013',
+    content:`   <p>
+                My Article.My Article.My Article.My Article.My Article.My Article.My Article.My Article.
+                    My Article.
+                        My Article.
+                            My Article.
+                                My Article.    My Article.
+            </p>
+            
+            <p>
+                My Article.My Article.My Article.My Article.My Article.My Article.My Article.
+                    My Article.
+                        My Article.
+                            My Article.
+                                My Article.    My Article.
+            </p>
+            
+            <p>
+                My Article.
+                    My Article.
+                        My Article.
+                            My Article.
+                                My Article.    My Article.
+            </p> `
+};
+
+
+function createTemplate(data){
+    var title;
+    var date;
+    var heading;
+    var content;
+    
+    
+var htmlTemplate=`
+<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viemport" content="width-device-width, initial-scale-1"/>
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr>
+        <h2>${heading}</h2>
+        
+        <div>
+        ${date}
+        </div>
+        <div>
+            ${content1}
+        </div>
+        </div>
+    </body>
+</html>
+
+
+
+`;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
