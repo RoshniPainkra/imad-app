71794323181455568746c130c5609d0aa530ccfd
RoshniPainkra <30731068+RoshniPainkra@users.noreply.github.com>
2017-08-25 15:27:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,11 @@ console.log('Loaded!');
 //Change text on maint
 
 var ele=document.getElementById("maint");
-ele.innerHTML="Roshni Roshni"
+ele.innerHTML="Roshni Roshni";
+
+//Move the image
+var img=document.getElementById("img");
+img.onClick= function()
+{
+    img.style.marginLeft= '100px';
+};
