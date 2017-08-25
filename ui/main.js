b00a5b5b3b001a3ea9301fc0d680ec1a2bf5b5e2
RoshniPainkra <30731068+RoshniPainkra@users.noreply.github.com>
2017-08-25 15:39:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,15 @@ ele.innerHTML="Roshni Roshni";
 
 //Move the image
 var img=document.getElementById("modi");
+var marginLeft=0;
+
+function moveRight()
+{
+    marginLeft= marginLeft+10;
+    img.style.marginLeft= marginLeft+'px';
+}
+
 img.onclick= function()
 {
-    img.style.marginLeft= '100px';
+    var interval=setinterval(moveRight,100);
 };
