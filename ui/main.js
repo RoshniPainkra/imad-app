f44f329dbe4fc90188cfb2384481d6a7650e624e
RoshniPainkra <30731068+RoshniPainkra@users.noreply.github.com>
2017-08-26 16:07:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,46 @@
-console.log('Loaded!');
+// console.log('Loaded!');
 
-//Change text on maint
+// //Change text on maint
 
-var ele=document.getElementById("maint");
-ele.innerHTML="Roshni Roshni";
+// var ele=document.getElementById("maint");
+// ele.innerHTML="Roshni Roshni";
 
-//Move the image
-var img=document.getElementById("modi");
-var marginLeft=0;
+// //Move the image
+// var img=document.getElementById("modi");
+// var marginLeft=0;
 
-function moveRight()
+// function moveRight()
+// {
+//     marginLeft= marginLeft + 1;
+//     img.style.marginLeft= marginLeft+'px';
+// }
+
+// img.onclick= function()
+// {
+//     var interval=setInterval(moveRight,50);
+// };
+
+
+
+
+//Counter
+var button=document.getElementById('counter');
+var counter=0;
+button.onclik=function()
 {
-    marginLeft= marginLeft + 1;
-    img.style.marginLeft= marginLeft+'px';
-}
-
-img.onclick= function()
-{
-    var interval=setInterval(moveRight,50);
+    //make a request to th counter endpoint
+    
+    
+    //capture the response and store it in variable
+    
+    
+    
+    //render the variable in correct span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML= counter.toString();
+    
+    
+    
+    
 };
